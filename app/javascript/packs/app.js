6c3a6087eeecddbbccb2dd401b793488bbf9a488
lizpeng.tw <lizpeng.tw@gmail.com>
2019-12-01 19:00:18 -0500

    Update toggleDone to use API

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -57,11 +57,16 @@ document.addEventListener("DOMContentLoaded", () => {
 			},
 			toggleDone: function(event, id) {
 				event.stopImmediatePropagation();
-				event.preventDefault();
 				let task = this.tasks.find(item => item.id == id);
 				if(task) {
 					task.completed = !task.completed;
-					this.message = `Task ${id} updated.`;
+					this.task = task;
+					Api.updateTask(this.task).then(function(response) {
+						app.listTasks();
+						app.clear();
+						let status = response.completed ? 'completed' : 'in progress';
+						app.message = `Task ${response.id} is ${status}.`;
+					})
 				}
 			},
 			createTask: function(event) {
